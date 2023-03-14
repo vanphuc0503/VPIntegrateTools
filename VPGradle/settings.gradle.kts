6acff27920315c14d904097c6645a51c11ebf1fc
vanphuc0503 <vanphuc.earn.money@gmail.com>
2023-03-14 14:40:16 +0700

    define_a_project_structure

---
+++ VPGradle/settings.gradle.kts
@@ -0,0 +1,29 @@
+
+pluginManagement {
+    repositories.gradlePluginPortal()
+//    repositories.mavenCentral()
+//    repositories.google()
+//    repositories.maven("https://my.location/repo") {
+//        credentials.username = "..."
+//        credentials.password = "..."
+//    }
+
+//    includeBuild("gradle/plugins")
+}
+
+dependencyResolutionManagement {
+    repositories.mavenCentral()
+//    repositories.google()
+//    repositories.maven("https://my.location/repo") {
+//        credentials.username = "..."
+//        credentials.password = "..."
+//    }
+
+//    includeBuild("../my-other-project")
+}
+
+rootProject.name = "VPGradle"
+
+include("app")
+include("business-logic")
+include("data-model")
