cb480076ef986336dff3d68efd394ebceb455de5
vanphuc0503 <vanphuc.earn.money@gmail.com>
2023-03-14 14:55:33 +0700

    subproject_build_file

---
+++ VPGradle/data-model/src/main/java/com/example/MesssageModel.java
@@ -0,0 +1,13 @@
+package com.example;
+
+public class MesssageModel {
+    private final String message;
+
+    public MesssageModel(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
